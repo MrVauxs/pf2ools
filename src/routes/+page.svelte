cdf009e857e416b2ebed063f50b3630de5fb1362
mrvauxs <mrvauxs@gmail.com>
2023-03-13 21:27:11 +0000

    create tag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,10 @@
 	<h1>Welcome to Home</h1>
 	<p>
 		Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a>
-		to read the documentation
+		to read the documentation for SvelteKit.
+	</p>
+	<p>
+		Visit <a href="https://github.com/MrVauxs/pf2ools">pf2ools GitHub repository</a>
+		to see the code for pf2ools.
 	</p>
 </div>
